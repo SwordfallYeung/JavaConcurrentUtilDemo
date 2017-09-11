73ec40f8f977a6c4a8d9771a6ef54399c425b4f1
GodYang <2506597416@qq.com>
2017-09-11 23:41:10 +0800

    提交RecursiveTask示例

--- src/com/util/concurrent/e_Forkjoin/RecursiveTaskExample.java
+++ src/com/util/concurrent/e_Forkjoin/RecursiveTaskExample.java
@@ -1,5 +1,11 @@
 package com.util.concurrent.e_Forkjoin;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
 import java.util.concurrent.RecursiveTask;
 
 /**
@@ -12,31 +18,87 @@ import java.util.concurrent.RecursiveTask;
  **/
 public class RecursiveTaskExample {
 
-	public static void main(String[] args) {
 
+	public static class CountTask extends RecursiveTask<Integer>{
+		private static final long serialVersionUID=-3611254198265061729L;
+
+		public static final int threshold=2;
+
+		private int start;
+
+		private int end;
+
+		public CountTask(int start, int end) {
+			this.start = start;
+			this.end = end;
+		}
+
+		@Override
+		protected Integer compute() {
+
+			int sum = 0;
+
+			//如果任务足够小就计算任务
+			boolean canCompute = (end -start) <=threshold;
+			if (canCompute){
+				for (int i=start;i<=end;i++){
+					sum+=i;
+				}
+			}else {
+				//如果任务大于阈值，就分裂成两个子任务计算
+				int middle=(start+end)/2;
+				CountTask leftTask=new CountTask(start,middle);
+				CountTask rightTask=new CountTask(middle+1,end);
+
+				//方式一：任务量小则采用fork()和join()函数
+				//执行子任务
+				leftTask.fork();
+				rightTask.fork();
+
+				System.out.println(Thread.currentThread().getName());
+
+				//等待任务执行结束合并其结果
+				int leftResult=leftTask.join();
+				int rightResult=rightTask.join();
+
+				//合并子任务
+				sum=leftResult+rightResult;
+
+				//方式二：任务量大则采用集合和invokeAll()一起执行, invokeAll()是对fork()和join()方法的封装
+				/*List<CountTask> list=new ArrayList<>();
+				list.add(leftTask);
+				list.add(rightTask);
+				Collection<CountTask> countTaskCollection= invokeAll(list);
+
+				for (CountTask c:countTaskCollection){
+					try {
+						sum+=c.get();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} catch (ExecutionException e) {
+						e.printStackTrace();
+					}
+				}*/
+			}
+			return sum;
+		}
 	}
 
-	public class Fibonacci extends RecursiveTask{
-		final int n;
+	public static void main(String[] args) {
+		ForkJoinPool forkJoinPool=new ForkJoinPool();
 
-		public Fibonacci(int n) {
-			this.n = n;
-		}
+		//生成一个计算任务，计算1+2+3+4
+		CountTask task=new CountTask(1,100);
 
-		protected int compute(int small) {
-			final int[] results={ 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89};
-			return results[small];
-		}
+		//执行一个任务
+		Future<Integer> result=forkJoinPool.submit(task);
 
-		public Integer compute(){
-			if (n<=10){
-				return compute(n);
-			}
-			Fibonacci f1=new Fibonacci(n-1);
-			Fibonacci f2=new Fibonacci(n-2);
-			f1.fork();
-			f2.fork();
-			return f1.join()+f2.join();
+		try {
+			System.out.println(result.get());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
 		}
 	}
 
