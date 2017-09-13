a145499d8ad4589e169a06ef3cd3857b184ccf78
GodYeung <2506597416@qq.com>
2017-09-13 15:59:32 +0800

    提交AtomicMarkableReference实例

---
+++ src/com/util/concurrent/z_atomic/AtomicMarkableReferenceExample.java
@@ -0,0 +1,11 @@
+package com.util.concurrent.z_atomic;
+
+/**
+ * @author y15079
+ * @create 2017-09-13 15:38
+ * @desc
+ *
+ * AtomicMarkableReference  相当于一个[引用,boolean]的二元组,可用于表示如：已删除的节点
+ **/
+public class AtomicMarkableReferenceExample {
+}
