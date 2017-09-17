c9e9cab2a6c85157d1fbf70ae212add37cc93062
GodYang <2506597416@qq.com>
2017-09-18 00:24:56 +0800

    EventListener实例

---
+++ src/com/util/base/EventListener.java
@@ -0,0 +1,12 @@
+package com.util.base;
+
+/**
+ * @author y15079
+ * @create: 9/18/17 12:24 AM
+ * @desc:
+ *
+ * EventListener.java 下面是所有超级接口EventListener.java的源代码   其实就是一个空接口.
+ *
+ */
+public class EventListener {
+}
