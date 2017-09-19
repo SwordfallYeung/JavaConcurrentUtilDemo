7d54a158792bfd9c46154199bdbb040acf02db8b
GodYang <2506597416@qq.com>
2017-09-20 00:31:38 +0800

    AbstractQueue抽象类

---
+++ src/com/util/base/b_class/AbstractQueueExample.java
@@ -0,0 +1,14 @@
+package com.util.base.b_class;
+
+/**
+ * @author y15079
+ * @create: 9/20/17 12:15 AM
+ * @desc:
+ *
+ * AbstractQueue<E>实现了Queue<E>接口，在这个抽象类中对于Queue<E>中定义的方法签名进行了简单的实现，
+ *
+ * 对于Queue<E>持有的两种形式的操作方法进行了相互的调用:add , remove, element
+ *
+ */
+public class AbstractQueueExample {
+}
