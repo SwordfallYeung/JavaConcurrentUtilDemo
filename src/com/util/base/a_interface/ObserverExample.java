9f356f0aac345197630ad003304752edcfb4d92e
GodYeung <2506597416@qq.com>
2017-09-19 10:54:33 +0800

    修改代码，使之符合规范

--- src/com/util/base/a_interface/ObserverExample.java
+++ src/com/util/base/a_interface/ObserverExample.java
@@ -49,8 +49,10 @@ public class ObserverExample {
     public static class NumObserver implements Observer{
         @Override
         public void update(Observable o, Object arg) {
-            NumObservable myObserable=(NumObservable) o;
-            System.out.println("Data has changed to "+myObserable.getData());
+            if (o instanceof NumObservable){
+                NumObservable myObservable=(NumObservable) o;
+                System.out.println("Data has changed to "+myObservable.getData());
+            }
         }
     }
 
