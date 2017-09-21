4563396afa46bfa1d9d5e93d75c8ae364522fdb2
GodYang <2506597416@qq.com>
2017-09-22 02:34:59 +0800

    Dictionary实例.HashTable的父类

---
+++ src/com/util/base/b_class/DictionaryExample.java
@@ -0,0 +1,15 @@
+package com.util.base.b_class;
+
+/**
+ * @author y15079
+ * @create: 9/22/17 2:33 AM
+ * @desc:
+ *
+ * Dictionary 类是一个抽象类，用来存储键/值对，作用和Map类相似
+ *
+ * Dictionary类已经过时了。在实际开发中，你可以实现Map接口来获取键/值的存储功能。
+ *
+ * Dictionary是HashTable的父类
+ */
+public class DictionaryExample {
+}
