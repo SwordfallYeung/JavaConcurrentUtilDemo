e8e2124c92aca1991f762a8f48adff8df79acb9d
GodYeung <2506597416@qq.com>
2017-09-23 14:54:21 +0800

    事件监听器接口

---
+++ src/com/util/base/a_interface/EventListenerExample.java
@@ -0,0 +1,12 @@
+package com.util.base.a_interface;
+
+/**
+ * @author y15079
+ * @create: 9/18/17 12:24 AM
+ * @desc:
+ *
+ * EventListenerExample.java 下面是所有超级接口EventListener.java的源代码   其实就是一个空接口.
+ *
+ */
+public class EventListenerExample {
+}
