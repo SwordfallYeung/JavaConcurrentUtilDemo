e732e901c7771ddd250180515d47929bdb7c5a0b
GodYang <2506597416@qq.com>
2017-09-25 00:40:46 +0800

    LongSummaryStatistics，Long类型的统计数据

---
+++ src/com/util/base/b_class/LongSummaryStatisticsExample.java
@@ -0,0 +1,12 @@
+package com.util.base.b_class;
+
+/**
+ * @author y15079
+ * @create: 9/25/17 12:35 AM
+ * @desc:
+ *
+ * Long类型的数据统计，可以参考DoubleSummaryStatistics和IntSummaryStatistics
+ *
+ */
+public class LongSummaryStatisticsExample {
+}
